cf1ac681b5a72e38c66a4e0ee8e69ab83c476bd6
drili <DriliB@hotmail.com>
2023-09-05 21:10:25 +0200

    15:35 Next.js Layouts and Components

--- next_js_app/src/app/page.jsx
+++ next_js_app/src/app/page.jsx
@@ -4,7 +4,7 @@ import styles from './page.module.css'
 export default function Home() {
     return (
         <div>
-            <h1>Hello</h1>
+            <h1>Home Page</h1>
         </div>
     )
 }
