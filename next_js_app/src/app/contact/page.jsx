997b9c54ec1de8072cd5572ee20517076ff6280b
drili <DriliB@hotmail.com>
2023-09-08 11:27:25 +0200

    WIP: 1:12:10 Next.js App UI Design

--- next_js_app/src/app/contact/page.jsx
+++ next_js_app/src/app/contact/page.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import styles from "./page.module.css"
+import Image from 'next/image'
+import Button from '@/components/button/Button'
 
 const Contact = () => {
     return (
-        <div className={styles.container}>Contact</div>
+        <div className={styles.container}>
+            <h1 className={styles.title}>Lorem, ipsum dolor.</h1>
+            <div className={styles.content}>
+                <div className={styles.imgContainer}>
+                    <Image
+                        src="/contact.png"
+                        alt=""
+                        fill={true}
+                        className={styles.image}
+                    />
+                </div>
+
+                <form className={styles.form} action="">
+                    <input type='text' placeholder='name' className={styles.input}></input>
+                    <input type='text' placeholder='email' className={styles.input}></input>
+                    <textarea className={styles.textArea} placeholder='message' name="" id="" cols="30" rows="10"></textarea>
+                    <Button
+                        url="#"
+                        text="Send"
+                    ></Button>
+                </form>
+            </div>
+        </div>
     )
 }
 
