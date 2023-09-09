f512e3490301909cb8a86a9feb03a1891834d04a
drili <DriliB@hotmail.com>
2023-09-09 13:23:42 +0200

    Next.js App UI Design

--- next_js_app/src/app/blog/page.jsx
+++ next_js_app/src/app/blog/page.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
 import styles from "./page.module.css"
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Blog = () => {
   return (
-    <div className={styles.container}>Blog</div>
+    <div className={styles.mainContainer}>
+        <Link href="/blog/testId" className={styles.container}>
+            <div className={styles.imageContainer}>
+                <Image
+                    src="https://images.pexels.com/photos/17427379/pexels-photo-17427379/free-photo-of-a-pelican-by-a-sea.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+                    alt=''
+                    width={400}
+                    height={250}
+                    className={styles.image}
+                ></Image>
+            </div>
+
+            <div className={styles.content}>
+                <h1 className={styles.title}>Test</h1>
+                <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti corrupti id obcaecati, cum temporibus laudantium?</p>
+            </div>
+        </Link>
+
+        <Link href="/blog/testId" className={styles.container}>
+            <div className={styles.imageContainer}>
+                <Image
+                    src="https://images.pexels.com/photos/17427379/pexels-photo-17427379/free-photo-of-a-pelican-by-a-sea.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+                    alt=''
+                    width={400}
+                    height={250}
+                    className={styles.image}
+                ></Image>
+            </div>
+
+            <div className={styles.content}>
+                <h1 className={styles.title}>Test</h1>
+                <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti corrupti id obcaecati, cum temporibus laudantium?</p>
+            </div>
+        </Link>
+    </div>
   )
 }
 
--- next_js_app/src/app/portfolio/[category]/page.jsx
+++ next_js_app/src/app/portfolio/[category]/page.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import styles from "./page.module.css"
+import Button from '@/components/button/Button'
+import Image from 'next/image'
 
-const Category = () => {
+const Category = ({ params }) => {
     return (
-        <div>Category</div>
+        <div>
+            <h1 className={styles.catTitle}>{params.category}</h1>
+
+            <div className={styles.item}>
+                <div className={styles.content}>
+                    <h1 className={styles.title}>Test</h1>
+                    <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, perferendis.</p>
+                    <Button
+                        url="/"
+                        text="See more"
+                    ></Button>
+                </div>
+                <div className={styles.imgContainer}>
+                    <Image
+                        className={styles.img}
+                        src="https://images.pexels.com/photos/17427379/pexels-photo-17427379/free-photo-of-a-pelican-by-a-sea.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+                        alt=""
+                        fill={true}
+                    ></Image>
+                </div>
+
+            </div>
+        </div>
     )
 }
 
