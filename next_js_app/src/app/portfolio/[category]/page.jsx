7f9e9c743e8bed155a1b78ce34b31e9b02fb9022
drili <DriliB@hotmail.com>
2023-09-15 20:30:20 +0200

    01:51:04 Next.js How to Fetch Data from Local Json File?

--- next_js_app/src/app/portfolio/[category]/page.jsx
+++ next_js_app/src/app/portfolio/[category]/page.jsx
@@ -2,31 +2,47 @@ import React from 'react'
 import styles from "./page.module.css"
 import Button from '@/components/button/Button'
 import Image from 'next/image'
+import { items } from './data.js'
+import { notFound } from "next/navigation"
+
+const getData = (cat) => {
+    const data = items[cat]
+
+    if(data) {
+        return data
+    }
+
+    return notFound()
+}
 
 const Category = ({ params }) => {
+    const data = getData(params.category)
+
     return (
         <div>
             <h1 className={styles.catTitle}>{params.category}</h1>
 
-            <div className={styles.item}>
-                <div className={styles.content}>
-                    <h1 className={styles.title}>Test</h1>
-                    <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, perferendis.</p>
-                    <Button
-                        url="/"
-                        text="See more"
-                    ></Button>
+            {data.map(item => (
+                <div key={item.id} className={styles.item}>
+                    <div className={styles.content}>
+                        <h1 className={styles.title}>{item.title}</h1>
+                        <p className={styles.desc}>{item.description}</p>
+                        <Button
+                            url="/"
+                            text="See more"
+                        ></Button>
+                    </div>
+                    <div className={styles.imgContainer}>
+                        <Image
+                            className={styles.img}
+                            src={item.image}
+                            alt=""
+                            fill={true}
+                        ></Image>
+                    </div>
                 </div>
-                <div className={styles.imgContainer}>
-                    <Image
-                        className={styles.img}
-                        src="https://images.pexels.com/photos/17427379/pexels-photo-17427379/free-photo-of-a-pelican-by-a-sea.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
-                        alt=""
-                        fill={true}
-                    ></Image>
-                </div>
-
-            </div>
+            ))}
+            
         </div>
     )
 }
