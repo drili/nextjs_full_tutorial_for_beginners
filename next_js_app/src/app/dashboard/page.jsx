1d338590a271dd3781dff04e20a7f2547b3eca29
drili <DriliB@hotmail.com>
2023-09-15 20:22:38 +0200

    :hammer: 01:45:50 Next.js How to Fetch Data on the Client Side? (SWR Tutorial)

--- next_js_app/src/app/dashboard/page.jsx
+++ next_js_app/src/app/dashboard/page.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect, useState } from 'react'
+import useSWR from 'swr'
 
 const Dashboard = () => {
+    // *** useEffect() example:
+    // useEffect(() => {
+    //     const getData = async () => {
+    //         setIsLoading(true)
+
+    //         const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    //             cache: "no-store"
+    //         })
+
+    //         if(!res.ok) {
+    //             // throw new Error("::: Failed to getData()")
+    //             setErr(true)
+    //         }
+
+    //         const data = await res.json()
+
+    //         setIsLoading(false)
+    //         setData(data)
+    //     }
+
+    //     getData()
+    // }, [])
+
+    const fetcher = (...args) => fetch(...args).then(res => res.json())
+    const { data, error, isLoading } = useSWR("https://jsonplaceholder.typicode.com/posts", fetcher)
+
+    console.log({ data })
+
     return (
-        <div>Dashboard</div>
+        <div>Dashboard 1</div>
     )
 }
 
