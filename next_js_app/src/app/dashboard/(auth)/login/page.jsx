34cf7454b44d1fedea3bb5362533ed19200fd66e
drili <DriliB@hotmail.com>
2023-12-25 15:47:53 +0100

    :tada: 02:16:50 Next.js Google Auth (Sign in with Google)

--- next_js_app/src/app/dashboard/(auth)/login/page.jsx
+++ next_js_app/src/app/dashboard/(auth)/login/page.jsx
@@ -1,9 +1,15 @@
+"use client"
+
 import React from 'react'
+import { signIn } from 'next-auth/react'
+
 import styles from "./page.module.css"
 
 const Login = () => {
   return (
-    <div className={styles.container}>Login</div>
+    <div className={styles.container}>
+        <button onClick={() => signIn("google")}>Login with Google</button>
+    </div>
   )
 }
 
--- next_js_app/src/app/dashboard/page.jsx
+++ next_js_app/src/app/dashboard/page.jsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { useSession } from 'next-auth/react'
 import React, { useEffect, useState } from 'react'
 import useSWR from 'swr'
 
@@ -27,10 +28,10 @@ const Dashboard = () => {
     //     getData()
     // }, [])
 
+    const session = useSession()
+
     const fetcher = (...args) => fetch(...args).then(res => res.json())
     const { data, error, isLoading } = useSWR("https://jsonplaceholder.typicode.com/posts", fetcher)
-
-    console.log({ data })
 
     return (
         <div>Dashboard 1</div>
